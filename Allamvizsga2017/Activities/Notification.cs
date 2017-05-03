469bbdfbf725aa1b5833a80b5feca3e43b9bc14c
Zsebe Akos <zsebea@yahoo.com>
2017-05-04 00:07:42 +0300

    notification added

--- Allamvizsga2017/Activities/Notification.cs
+++ Allamvizsga2017/Activities/Notification.cs
@@ -1,15 +1,19 @@
 ﻿
+using Allamvizsga2017.Models;
 using Android.App;
+using Android.Content;
 using Android.OS;
 using Android.Widget;
+using Newtonsoft.Json;
 using Quobject.SocketIoClientDotNet.Client;
+using System.Collections.Generic;
 
 namespace Allamvizsga2017.Activities
 {
     [Activity(Label = "Notification", MainLauncher = true)]
     public class Notification : Activity
     {
-        public Socket socket = IO.Socket("http://allamvizsga-akoszsebe.c9users.io:8080");
+        public Socket socket = IO.Socket("http://allamvizsga-akoszsebe.c9users.io");
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -19,14 +23,34 @@ namespace Allamvizsga2017.Activities
             TextView text = FindViewById<TextView>(Resource.Id.textView1);
             connect.Click += delegate
             {
-                socket.Connect().Emit("register", "{ \"id\" : \"12\" , \"house_ids\" : [ \"0\", \"1\" ]}");
+                socket.Connect().Emit("register", "{ \"id\" : \"12\" , \"house_ids\" : [ \"1\" ]}");
             };
-            
 
+            int notificationId = 0;
             socket.On("notification", data =>
             {
+                var d = JsonConvert.DeserializeObject<List<Device>>(data.ToString());
                 RunOnUiThread(()=> {
-                    text.Text = data.ToString() + '\n';
+                    //text.Text = data.ToString() + '\n';
+                    // Instantiate the builder and set notification elements:
+                    Android.App.Notification.Builder builder = new Android.App.Notification.Builder(this)
+                        .SetContentTitle("Turned On")
+                        .SetContentText(d[0].name + " " + d[0].value + " W")
+                        .SetSmallIcon(d[0].icon_id)
+                        .SetDefaults(NotificationDefaults.Sound);
+
+                    // Build the notification:
+                    Android.App.Notification notification = builder.Build();
+
+                    // Get the notification manager:
+                    NotificationManager notificationManager =
+                        GetSystemService(Context.NotificationService) as NotificationManager;
+
+                    // Publish the notification:
+                    
+                    notificationManager.Notify(notificationId, notification);
+                    notificationId++;
+                    if (notificationId == 10) notificationId = 0;
                 });
                 
             });
